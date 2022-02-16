1cbcd11ff059492f0c67b66bfc7fdbf39b627904
Vitória Santos Oliveira <contato.santos.vitoria12@gmail.com>
2022-02-15 21:42:39 -0300

    adicionando navbar, home e footer

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import {Box, Button, Paper} from '@material-ui/core'
+import { Box, Grid, Typography, Button, Paper } from '@material-ui/core'
 import './Home.css';
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-            <Paper>
-                <Box p={2}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
                     <Box display="flex" justifyContent="center">
-                        <h1>Titulo</h1>
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
                     </Box>
-                    <img src="https://storage.googleapis.com/atados-v3/user-uploaded/images/8010b1b6-32bc-4dd4-bc11-a4a607c9974c.png" alt="logo" style={{width:"100%", height:"100%"}}/>
-                    <Box display="flex" justifyContent="center" p={2}>
-                        <Button variant = "contained" color="primary">texto1</Button>
-                        <Button variant = "contained" color="secondary">texto2</Button>
-                    </Box>
-                </Box>
-            </Paper>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
